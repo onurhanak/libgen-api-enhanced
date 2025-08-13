735bc73e39cc9689837d8b1484494ac1a2bfa196
Onurhan <onurhanak@protonmail.com>
2025-08-13 12:50:55 -0400

    added direct tor download link

--- libgen_api_enhanced/search_request.py
+++ libgen_api_enhanced/search_request.py
@@ -14,7 +14,6 @@ import json
 
 
 class SearchRequest:
-
     col_names = [
         "ID",
         "Title",
@@ -47,17 +46,12 @@ class SearchRequest:
     def get_search_page(self):
         query_parsed = "%20".join(self.query.split(" "))
         if self.search_type.lower() == "title":
-            search_url = (
-                f"https://libgen.li/index.php?req={query_parsed}&columns%5B%5D=t&objects%5B%5D=f&objects%5B%5D=e&objects%5B%5D=s&objects%5B%5D=a&objects%5B%5D=p&objects%5B%5D=w&topics%5B%5D=l&res=100&filesuns=all"
-            )
+            search_url = f"https://libgen.li/index.php?req={query_parsed}&columns%5B%5D=t&objects%5B%5D=f&objects%5B%5D=e&objects%5B%5D=s&objects%5B%5D=a&objects%5B%5D=p&objects%5B%5D=w&topics%5B%5D=l&res=100&filesuns=all"
         elif self.search_type.lower() == "author":
-            search_url = (
-                f"https://libgen.li/index.php?req={query_parsed}&columns%5B%5D=a&objects%5B%5D=f&objects%5B%5D=e&objects%5B%5D=s&objects%5B%5D=a&objects%5B%5D=p&objects%5B%5D=w&topics%5B%5D=l&res=100&filesuns=all"
-            )
+            search_url = f"https://libgen.li/index.php?req={query_parsed}&columns%5B%5D=a&objects%5B%5D=f&objects%5B%5D=e&objects%5B%5D=s&objects%5B%5D=a&objects%5B%5D=p&objects%5B%5D=w&topics%5B%5D=l&res=100&filesuns=all"
         elif self.search_type.lower() == "default":
-            sesarch_url = (
-                f"https://libgen.li/index.php?req={query_parsed}&columns%5B%5D=t&columns%5B%5D=a&columns%5B%5D=s&columns%5B%5D=y&columns%5B%5D=p&columns%5B%5D=i&objects%5B%5D=f&objects%5B%5D=e&objects%5B%5D=s&objects%5B%5D=a&objects%5B%5D=p&objects%5B%5D=w&topics%5B%5D=l&res=100&filesuns=all"
-            )
+            search_url = f"https://libgen.li/index.php?req={query_parsed}&columns%5B%5D=t&columns%5B%5D=a&columns%5B%5D=s&columns%5B%5D=y&columns%5B%5D=p&columns%5B%5D=i&objects%5B%5D=f&objects%5B%5D=e&objects%5B%5D=s&objects%5B%5D=a&objects%5B%5D=p&objects%5B%5D=w&topics%5B%5D=l&res=100&filesuns=all"
+
         if self.mirror == "https://libgen.is/":
             if self.search_type.lower() == "title":
                 search_url = f"https://libgen.is/search.php?req={query_parsed}&column=title&res=100"
@@ -83,25 +77,23 @@ class SearchRequest:
     def add_direct_download_links(self, output_data):
         # Add a direct download link to each result
         for book in output_data:
-            id = book["ID"]
-            download_id = str(id)[:-3] + "000"
             md5 = book["Mirror_1"].split("/")[-1].lower()
             title = urllib.parse.quote(book["Title"])
             extension = book["Extension"]
-            book["Direct_Download_Link"] = (
-                f"https://download.books.ms/main/{download_id}/{md5}/{title}.{extension}"
+            book["Direct_Download_Tor_Link"] = (
+                f"http://libgenfrialc7tguyjywa36vtrdcplwpxaw43h6o63dmmwhvavo5rqqd.onion/LG/01311000/2d6d1cf09a327162d4230afde4413a39/{title}.{extension}"
             )
 
         return output_data
 
     def add_book_cover_links(self, output_data):
-
         ids = ",".join([book["ID"] for book in output_data])
 
-        url = f"https://libgen.li/json.php?ids={ids}&fields=id,md5,openlibraryid"
+        url = f"https://libgen.bz/json.php?ids={ids}&fields=id,md5,openlibraryid"
+        print(url)
 
         response = json.loads(requests.get(url).text)
-        print(response.text)
+
         # match openlibraryid to id
         for book in output_data:
             for book_json in response:
@@ -117,18 +109,11 @@ class SearchRequest:
         soup = BeautifulSoup(search_page.text, "html.parser")
         self.strip_i_tag_from_soup(soup)
 
-        # Libgen results contain 3 tables
-        # Table2: Table of data to scrape.
         information_table = soup.find_all("table")[1]
-        # Determines whether the link url (for the mirror)
-        # or link text (for the title) should be preserved.
-        # Both the book title and mirror links have a "title" attribute,
-        # but only the mirror links have it filled.(title vs title="libgen.io")
 
         raw_data = [
             [
-<<<<<<< HEAD
-                values[0].find("a", href=True)['href'].split("id=")[-1],
+                values[0].find("a", href=True)["href"].split("id=")[-1],
                 values[0].text,
                 values[1].text,
                 values[2].text,
@@ -137,32 +122,21 @@ class SearchRequest:
                 values[6].text,
                 values[7].text,
                 *[
-                    a['href'] if a['href'][0] != "/" else f"https://libgen.li{a['href']}"
+                    (
+                        a["href"]
+                        if a["href"][0] != "/"
+                        else f"https://libgen.li{a['href']}"
+                    )
                     for a in values[8].find_all("a", href=True)
-                ]
-=======
-                (
-                    td.a["href"]
-                    if td.find("a")
-                    and td.find("a").has_attr("title")
-                    and td.find("a")["title"] != ""
-                    else "".join(td.stripped_strings)
-                )
-                for td in row.find_all("td")
->>>>>>> 024057b (working on alternative mirror support)
+                ],
             ]
             for row in information_table.find_all("tr")[1:]
             if (values := row.find_all("td"))
         ]
         output_data = [dict(zip(self.col_names, row)) for row in raw_data]
-<<<<<<< HEAD
-        #output_data = self.add_direct_download_links(output_data)
-        #output_data = self.add_book_cover_links(output_data)
-        return output_data[0]
-=======
         output_data = self.add_direct_download_links(output_data)
         output_data = self.add_book_cover_links(output_data)
-        return output_data
+        return output_data[0]
 
     def aggregate_request_data_libgen_alt(self):
         search_page = self.get_search_page()
@@ -171,21 +145,21 @@ class SearchRequest:
 
         information_table = soup.find("table", {"id": "tablelibgen"})
 
-        col_names = [
-            "ID",
-            "Title",
-            "Author",
-            "Publisher",
-            "Year",
-            "Language",
-            "Pages",
-            "Size",
-            "Extension",
-            "Mirror_1",
-            "Mirror_2",
-            "Mirror_3",
-            "Mirror_4",
-        ]
+        # col_names = [
+        #     "ID",
+        #     "Title",
+        #     "Author",
+        #     "Publisher",
+        #     "Year",
+        #     "Language",
+        #     "Pages",
+        #     "Size",
+        #     "Extension",
+        #     "Mirror_1",
+        #     "Mirror_2",
+        #     "Mirror_3",
+        #     "Mirror_4",
+        # ]
 
         all_rows = []
 
@@ -222,6 +196,8 @@ class SearchRequest:
                 while len(mirrors) < 4:
                     mirrors.append("")
 
+                md5 = mirrors[0].split("/")[-1]
+
                 all_rows.append(
                     [
                         id,
@@ -233,14 +209,15 @@ class SearchRequest:
                         pages,
                         size,
                         extension,
+                        md5,
                         *mirrors,
                     ]
                 )
 
             except Exception:
-                continue  # skip this row on any unexpected error
+                continue
 
-        output_data = [dict(zip(col_names, row)) for row in all_rows]
+        output_data = [dict(zip(self.col_names, row)) for row in all_rows]
 
         output_data = self.add_direct_download_links(output_data)
         # output_data = self.add_book_cover_links(output_data)
